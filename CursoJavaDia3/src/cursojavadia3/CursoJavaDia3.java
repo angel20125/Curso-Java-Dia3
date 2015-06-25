a388a20bdbf645e84858a74db47c7d8d7c924720
laboratorio <laboratorio@172.20.3.138>
2015-06-25 07:16:16 -0500

    Commit Inicial del curso de java del día 3,  Modificacion JFrame

--- CursoJavaDia3/src/cursojavadia3/CursoJavaDia3.java
+++ CursoJavaDia3/src/cursojavadia3/CursoJavaDia3.java
@@ -1,12 +1,30 @@
 
 package cursojavadia3;
 
+import javax.swing.JOptionPane;
+
 
 public class CursoJavaDia3 {
 
-  
+ 
     public static void main(String[] args) {
-       
-    }
+
     
+    }
 }
+ /*
+   JOptionPane.showMessageDialog(null, "hola mundo");
+   JOptionPane.showInputDialog("CUARSO JAVA");
+  */
+    /*
+            String valor = JOptionPane.showInputDialog("Di tu nombre");
+        
+        if(valor != null){
+            JOptionPane.showMessageDialog(null, "hola"
+            + valor
+            +"Mcuho gusto"
+            );
+        }
+  
+    }
+    */
